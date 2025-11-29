01a1856bff9a60663531fbd198df45e8666ba531
Mariana <mariana.iwaivk@gmail.com>
2025-11-29 06:46:51 -0300

    feat(search-field): add value prop to support controlled input (searchText)

--- src/components/SearchField/index.jsx
+++ src/components/SearchField/index.jsx
@@ -1,12 +1,13 @@
 import styles from "./SearchField.module.scss";
 import searchIcon from "../../assets/icons/lupa.png"
 
-const SearchField = ({ onSearch}) => {
+const SearchField = ({ searchText, onSearch}) => {
     return (
         <div className={styles.search}>
             <img src={searchIcon} className={styles.search__icon} />
             <input
                 type="text"
+                value={searchText}
                 onChange={(e) => onSearch(e.target.value)}
                 className={styles.search__input}
                 placeholder="Pesquise aqui um dos pratos do nosso cardápio"
