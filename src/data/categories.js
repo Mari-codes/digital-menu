8030d869eac0fd56a3cd4f267eab25a7fb72532d
Mariana <mariana.iwaivk@gmail.com>
2025-11-29 05:14:38 -0300

    feat(data): add categories data file

---
+++ src/data/categories.js
@@ -0,0 +1,15 @@
+import entradasIcon from "../assets/icons/entrada.png";
+import massasIcon from "../assets/icons/massa.png";
+import carnesIcon from "../assets/icons/carne.png";
+import bebidasIcon from "../assets/icons/bebidas.png";
+import saladasIcon from "../assets/icons/salada.png";
+import sobremesaIcon from "../assets/icons/sobremesa.png";
+
+export const categories = [
+  { id: 1, label: "Entradas", icon: entradasIcon },
+  { id: 2, label: "Massas", icon: massasIcon },
+  { id: 3, label: "Carnes", icon: carnesIcon },
+  { id: 4, label: "Bebidas", icon: bebidasIcon },
+  { id: 5, label: "Saladas", icon: saladasIcon },
+  { id: 6, label: "Sobremesas", icon: sobremesaIcon },
+];
